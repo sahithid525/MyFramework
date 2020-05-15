652b9fa376531b596ed36f65d8d07970f1633868
DSteps <dstepapps@gmail.com>
2020-05-15 19:39:17 +0530

    Initial Commit

---
+++ MyFramework/MyFramework/Service.swift
@@ -0,0 +1,17 @@
+//
+//  Service.swift
+//  MyFramework
+//
+//  Created by Sanket Deshmukh on 15/05/20.
+//  Copyright © 2020 Personal Org. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    private init(){}
+    
+    public static func doSomething() -> String{
+        return "MyMannn!!"
+    }
+}
